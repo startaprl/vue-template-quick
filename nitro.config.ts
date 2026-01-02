d3d3d905653e8535d9f6e8c30d5483993cf1fd71
startaprl <startaprl@163.com>
2026-01-02 23:01:59 +0800

    feat: 调整mock文件夹名称至server

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,6 +1,6 @@
 import { defineNitroConfig } from 'nitropack/config'
 
 export default defineNitroConfig({
-  // 将 mock 目录作为 Nitro 的源码目录，Nitro 会自动扫描该目录下的 api、routes 等文件夹
-  srcDir: 'mock',
+  // 将 server 目录作为 Nitro 的源码目录，Nitro 会自动扫描该目录下的 api、routes 等文件夹
+  srcDir: 'server',
 })
