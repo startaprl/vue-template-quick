b65a5fc04c853141c385cbb6e859efa2f407e0e3
startaprl <startaprl@163.com>
2026-01-02 22:36:43 +0800

    chore: 开发端口关联环境变量配置化

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,43 @@
 import path from 'node:path'
 import process from 'node:process'
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
-export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "@/styles/variable" as *;',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/styles/variable" as *;',
+        },
       },
     },
-  },
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(process.cwd(), 'src'),
-    },
-  },
-  server: {
-    port: 8888,
-    proxy: {
-      '/api': {
-        changeOrigin: true,
-        rewrite: path => path,
-        // rewrite: path => path.replace(/^\/api/, ''),
-        target: 'http://localhost:3000',
-        // ws: true,
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(process.cwd(), 'src'),
       },
     },
-  },
+    server: {
+      port: Number(env.VITE_PORT) || 5173,
+      proxy: {
+        '/api': {
+          changeOrigin: true,
+          rewrite: path => path,
+          // rewrite: path => path.replace(/^\/api/, ''),
+          target: 'http://localhost:3000',
+          ws: true,
+        },
+      },
+    },
+  }
 })
