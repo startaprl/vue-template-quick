1735a811e93d143d72bd5354dfd17414c6a61457
startaprl <startaprl@163.com>
2026-01-30 09:11:32 +0800

    chore: 移除preinstall脚本

--- scripts/preinstall.js
+++
@@ -1,8 +0,0 @@
-import process from 'node:process'
-
-if (!/pnpm/.test(process.env.npm_execpath || '')) {
-  console.warn(
-    `\u001B[33mThis repository must using pnpm as the package manager for scripts to work properly.\u001B[39m\n`,
-  )
-  process.exit(1)
-}
